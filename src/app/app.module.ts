51630de56db0b1e2509cd08dc9235b106c43db9f
mohamed <somboool25544@gmail.com>
2021-11-07 15:24:00 +0200

    deploy test to heroku

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { ProductsComponent } from './products/products.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
